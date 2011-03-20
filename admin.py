15c2deab038cb89bbd0716381679a02719ce3f4b
Hoang Xuan Phu <phunehehe@gmail.com>
2011-03-20 21:35:51 +0700

    add the unfinished admin module

---
+++ admin.py
@@ -0,0 +1,86 @@
+# The contents of this file are subject to the Mozilla Public License
+# Version 1.1 (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+# License for the specific language governing rights and limitations
+# under the License.
+#
+# The Original Code is GAAdmin.
+#
+# The Initial Developer of the Original Code is Hoang Xuan Phu.
+# Portions created by the Initial Developer are Copyright (C) 2011
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s): Hoang Xuan Phu.
+#
+# Alternatively, the contents of this file may be used under the terms
+# of the GNU General Public License version 3 or later
+# (the "GPL License"), in which case the
+# provisions of the GPL License are applicable instead of those
+# above.  If you wish to allow use of your version of this file only
+# under the terms of the GPL License and not to allow others to use
+# your version of this file under the MPL, indicate your decision by
+# deleting  the provisions above and replace  them with the notice and
+# other provisions required by the GPL License.  If you do not delete
+# the provisions above, a recipient may use your version of this file
+# under either the MPL or the GPL License.
+
+
+from zope.testbrowser.browser import Browser
+import itertools
+
+
+class Administrator():
+
+
+    def __init__(self, username, password, domain):
+        self.domain = domain
+        self.browser = Browser()
+        self.login(username, password)
+
+
+    def login(self, username, password):
+
+        self.browser.open('https://www.google.com/a/%s/ServiceLogin', self.domain)
+        form = browser.getForm(id='gaia_loginform')
+
+        # domain is automatically added
+        form.getControl(name='Email').value = username
+        form.getControl(name='Passwd').value = password
+        form.submit()
+
+
+    def go_to_group(self, group):
+        browser.open('https://www.google.com/a/cpanel/%s/Group?groupId=%s' % (self.domain, group))
+
+
+    def add_user_to_group(self, user, group):
+
+        self.go_to_group(group)
+
+        form = browser.getForm(id='addmember')
+        form.getControl(name='members').value = user
+        # Click this button to submit the form
+        form.getControl(name='add').click()
+
+
+    def user_is_in_group(self, user, group):
+
+        self.go_to_group(group)
+
+        try:
+
+            form = browser.getForm(id='list')
+            #for i in itertools.count():
+                #print i
+
+            # Go to next page and repeat
+            link_to_next_page = browser.getLink(text='Next')
+            link_to_next_page.click()
+
+        except LinkNotFoundError:
+            pass
+
